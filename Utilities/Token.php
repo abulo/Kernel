7e5be1825817aea886c7a02f0093617bca002280
abulo <abulo.hoo@gmail.com>
2018-06-29 11:10:35 +0800

    add token

--- Utilities/Token.php
+++ Utilities/Token.php
@@ -13,8 +13,17 @@ class Token
     public static function encode($string, $key = '')
     {
         $key = md5($key ? $key : 'weimeng');
-        $abc = @openssl_encrypt($string, 'des-ede3', $key, true);
-        return strtoupper(@bin2hex($abc));
+
+
+
+        $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+        $iv = openssl_random_pseudo_bytes($ivlen);
+        $ciphertext_raw = openssl_encrypt($string, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+        $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
+        $ciphertext = base64_encode($iv.$hmac.$ciphertext_raw);
+        return $ciphertext;
+        // $abc = @openssl_encrypt($string, 'des-ede3', $key, true);
+        // return strtoupper(@bin2hex($abc));
     }
     /**
      * 解密
@@ -25,14 +34,28 @@ class Token
     public static function decode($string, $key = '')
     {
         $key = md5($key ? $key : 'weimeng');
-        try {
-            $string=@pack("H*", strtolower($string));
-            $abc = @openssl_decrypt($string, 'des-ede3', $key, true);
-        } catch (\Exception $e) {
-            $abc = false;
+
+
+        $c = base64_decode($string);
+        $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+        $iv = substr($c, 0, $ivlen);
+        $hmac = substr($c, $ivlen, $sha2len = 32);
+        $ciphertext_raw = substr($c, $ivlen+$sha2len);
+        $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+        $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
+        if (hash_equals($hmac, $calcmac)) {//PHP 5.6+ timing attack safe comparison
+            return $original_plaintext;
+        } else {
+            return false;
         }
+        // try {
+        //     $string=@pack("H*", strtolower($string));
+        //     $abc = @openssl_decrypt($string, 'des-ede3', $key, true);
+        // } catch (\Exception $e) {
+        //     $abc = false;
+        // }
 
 
-        return  $abc;
+        // return  $abc;
     }
 }
